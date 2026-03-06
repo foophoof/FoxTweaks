2370f379ce4a2cf660f3b6e8caeafe78317126e0
foophoof <me@foophoof.com>
2026-03-06 01:31:18 +0000

    remove some leftovers from scaffolding

--- FoxTweaks/Plugin.cs
+++ FoxTweaks/Plugin.cs
@@ -39,19 +39,8 @@ namespace FoxTweaks {
 
     public override void ConfigureContainer(ContainerBuilder containerBuilder) {
       // While you can register services in the service collection, as long as you register a service as IHostedService(the AsImplementedInterfaces call) it will automatically be picked up by the host. This also avoids potential double registrations.
-      containerBuilder.RegisterType<CommandService>().AsSelf().AsImplementedInterfaces().SingleInstance();
-      containerBuilder.RegisterType<ConfigurationService>().AsSelf().AsImplementedInterfaces().SingleInstance();
-      containerBuilder.RegisterType<InstallerWindowService>().AsSelf().AsImplementedInterfaces().SingleInstance();
       containerBuilder.RegisterType<MiniMapService>().AsSelf().AsImplementedInterfaces().SingleInstance();
       containerBuilder.RegisterType<RandomClassService>().AsSelf().AsImplementedInterfaces().SingleInstance();
-      containerBuilder.RegisterType<WindowService>().AsSelf().AsImplementedInterfaces().SingleInstance();
-
-      // Register every class ending in Window inside our assembly with the container
-      containerBuilder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly())
-          .Where(t => t.Name.EndsWith("Window"))
-          .As<Window>()
-          .AsSelf()
-          .AsImplementedInterfaces();
 
       containerBuilder.Register(c => c.Resolve<IDataManager>().GameData).SingleInstance();
 
@@ -61,12 +50,6 @@ namespace FoxTweaks {
         var method = typeof(GameData).GetMethod(nameof(GameData.GetExcelSheet))?.MakeGenericMethod(parameters);
         return method!.Invoke(gameData, [null, null])!;
       }).As(typeof(ExcelSheet<>));
-
-      // Register the configuration with the container so that it's loaded/created when requested.
-      containerBuilder.Register(s => {
-        var configurationLoaderService = s.Resolve<ConfigurationService>();
-        return configurationLoaderService.GetConfiguration();
-      }).SingleInstance();
     }
 
     public override void ConfigureServices(IServiceCollection serviceCollection) {
