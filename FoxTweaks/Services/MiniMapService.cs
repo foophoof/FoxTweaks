1ab827cc2b2aad7d24abe0364ca9f05d7a9d5399
foophoof <me@foophoof.com>
2026-03-06 01:27:10 +0000

    move minimap overlay drawing to MiniMapService
    since the Draw method is already in an ImGui context, there's no need to spin up a whole window, thank to infi for pointing this out

--- FoxTweaks/Services/MiniMapService.cs
+++ FoxTweaks/Services/MiniMapService.cs
@@ -2,8 +2,10 @@ using System;
 using System.Numerics;
 using System.Threading;
 using System.Threading.Tasks;
+using Dalamud.Bindings.ImGui;
 using Dalamud.Game.ClientState.Objects.Enums;
 using Dalamud.Game.NativeWrapper;
+using Dalamud.Interface;
 using Dalamud.Plugin.Services;
 using FFXIVClientStructs.FFXIV.Component.GUI;
 using Lumina.Excel;
@@ -16,29 +18,33 @@ namespace FoxTweaks.Services {
                               ExcelSheet<Map> maps,
                               IClientState clientState,
                               IFramework framework,
-                              IPluginLog pluginLog) : IHostedService {
+                              IPluginLog pluginLog,
+                              IUiBuilder uiBuilder,
+                              IObjectTable objectTable) : IHostedService {
     private AtkUnitBasePtr NaviMapAddon => gameGui.GetAddonByName("_NaviMap");
-    private Map? currentMap = null;
+    private Map? _currentMap = null;
 
-    public float ZoneScale { get; private set; } = 1f;
-    public float NaviScale { get; private set; } = 1f;
-    public float Zoom { get; private set; } = 1f;
-    public float Rotation { get; private set; }
+    private float ZoneScale { get; set; } = 1f;
+    private float NaviScale { get; set; } = 1f;
+    private float Zoom { get; set; } = 1f;
+    private float Rotation { get; set; }
     public bool IsVisible { get; private set; }
-    public bool IsLocked { get; private set; }
-
-    public Vector2 MapPos => NaviMapAddon.Position;
-    public Vector2 MapSize => new(218 * NaviScale, 218 * NaviScale);
-
-    public Vector2 PlayerCirclePos => new(NaviMapAddon.X + MapSize.X / 2, NaviMapAddon.Y + MapSize.Y / 2);
+    private bool IsLocked { get; set; }
+    private Vector2 MapPos { get; set; }
+    private Vector2 MapSize { get; set; }
+    private Vector2 PlayerCirclePos { get; set; }
 
     public float MinimapScale => ZoneScale * NaviScale * Zoom;
+
+    private readonly uint _circleColor = ImGui.ColorConvertFloat4ToU32(new Vector4(0.957f, 0.533f, 0.051f, 1));
+    private readonly uint _borderColor = ImGui.ColorConvertFloat4ToU32(new Vector4(0.957f, 0.533f, 0.051f, 1) * 0.7f);
 
     public Task StartAsync(CancellationToken cancellationToken) {
       clientState.MapIdChanged += ClientStateOnMapIdChanged;
       ClientStateOnMapIdChanged(clientState.MapId);
 
       framework.Update += FrameworkOnUpdate;
+      uiBuilder.Draw += UiBuilderOnDraw;
 
       return Task.CompletedTask;
     }
@@ -46,16 +52,62 @@ namespace FoxTweaks.Services {
     public Task StopAsync(CancellationToken cancellationToken) {
       clientState.MapIdChanged -= ClientStateOnMapIdChanged;
       framework.Update -= FrameworkOnUpdate;
+      uiBuilder.Draw -= UiBuilderOnDraw;
 
       return Task.CompletedTask;
+    }
+
+    private void UiBuilderOnDraw() {
+      if (!IsVisible) {
+        return;
+      }
+
+      var drawList = ImGui.GetForegroundDrawList();
+      var windowPos = ImGui.GetWindowViewport().Pos;
+
+      var player = objectTable.LocalPlayer;
+      if (player is null) {
+        return;
+      }
+
+      var playerCirclePos = PlayerCirclePos + windowPos;
+      playerCirclePos.Y -= 5f;
+
+      var friends = objectTable.PlayerObjects.AsValueEnumerable(); //.Where(c => (c.StatusFlags & StatusFlags.Friend) != 0);
+      foreach (var battleChara in friends) {
+        if ((battleChara.StatusFlags & StatusFlags.AllianceMember) != 0 || (battleChara.StatusFlags & StatusFlags.PartyMember) != 0) {
+          continue;
+        }
+
+        var relativePersonPos = new Vector2(player.Position.X, player.Position.Z) - new Vector2(battleChara.Position.X, battleChara.Position.Z);
+        relativePersonPos *= MinimapScale;
+
+        if (!IsLocked) {
+          relativePersonPos = Vector2.Transform(relativePersonPos, Matrix3x2.CreateRotation(Rotation));
+        }
+
+        var personCirclePos = playerCirclePos - relativePersonPos;
+
+        float distance = Vector2.Distance(playerCirclePos, personCirclePos);
+
+        float minimapRadius = MapSize.X * 0.315f;
+        if (distance > minimapRadius) {
+          var originToObject = personCirclePos - playerCirclePos;
+          originToObject *= minimapRadius / distance;
+          personCirclePos = playerCirclePos + originToObject;
+        }
+
+        drawList.AddCircleFilled(personCirclePos, 6f, _circleColor);
+        drawList.AddCircle(personCirclePos, 6f, _borderColor, 0, 2);
+      }
     }
 
     private void ClientStateOnMapIdChanged(uint mapId) {
       if (mapId == 0) {
         return;
       }
-      currentMap = maps.GetRow(mapId);
-      ushort sizeFactor = currentMap?.SizeFactor ?? 100;
+      _currentMap = maps.GetRow(mapId);
+      ushort sizeFactor = _currentMap?.SizeFactor ?? 100;
       ZoneScale = sizeFactor / 100f;
     }
 
@@ -74,6 +126,9 @@ namespace FoxTweaks.Services {
 
       IsVisible = true;
       NaviScale = naviMapAddon.Scale;
+      MapSize = new Vector2(218, 218) * NaviScale;
+      MapPos = naviMapAddon.Position;
+      PlayerCirclePos = MapPos + MapSize / 2;
 
       UpdateZoom(naviMapAddon);
       UpdateRotation(naviMapAddon);
